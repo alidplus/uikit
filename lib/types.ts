690bb4ade0d9bf6f4175b17d88d92007c7972bcc
Ali Ghorbani <alidoubleplus@gmail.com>
2025-12-23 18:27:15 +0330

    wip dropdown

---
+++ lib/types.ts
@@ -0,0 +1,22 @@
+import type { Dispatch, ReactNode, SetStateAction } from "react";
+
+export interface DataProps<T> {
+  data: T;
+}
+
+export interface ValueProps<T> {
+  value?: T;
+}
+
+export interface InputProps<T> extends ValueProps<T> {
+  onChange: Dispatch<SetStateAction<T>>;
+}
+
+export interface IOptionValue<T extends string = string> {
+  value: T;
+  label?: string;
+  icon?: ReactNode;
+  text?: string;
+  disabled?: boolean;
+  onClick?: VoidFunction;
+}
